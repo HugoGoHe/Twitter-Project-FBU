5d233f27ec28b804a637f4a099b7c864cecd14b3
Hugo Alejandro Gomez Herrera <a01640856@itesm.mx>
2022-06-23 16:55:56 -0700

    Step 5.2 Favoriting
    - Favoriting feature implemented

--- twitter/Cells/TweetCell.m
+++ twitter/Cells/TweetCell.m
@@ -5,7 +5,7 @@
 //  Created by Hugo Gomez Herrera on 6/22/22.
 //  Copyright © 2022 Emerson Malca. All rights reserved.
 //
-
+#import "APIManager.h"
 #import "TweetCell.h"
 
 @implementation TweetCell
@@ -19,6 +19,46 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+}
+
+- (IBAction)didTapFavorite:(id)sender {
+    // Update the local tweet model
+    if (self.tweet.favorited){
+        self.tweet.favorited = NO;
+        self.tweet.favoriteCount -= 1;
+        [self refreshData];
+        // Send a POST request to the POST favorites/create endpoint
+        [[APIManager shared] unfavorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error unfavoriting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully unfavorited the following Tweet: %@", tweet.text);
+            }
+        }];
+        
+    } else{
+        self.tweet.favorited = YES;
+        self.tweet.favoriteCount += 1;
+        [self refreshData];
+        // Send a POST request to the POST favorites/create endpoint
+        [[APIManager shared] favorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                 NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully favorited the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+
+}
+
+-(void)refreshData{
+
+    self.favoriteCountLabel.text =[@(self.tweet.favoriteCount) stringValue];
+    
+    
 }
 
 @end
